2c18cb6df1839417f9ba878a92b792b3d54cf35c
jay311311 <jay311311@gmail.com>
2020-08-11 17:21:37 +0900

    포트폴리오 3차

--- javascript/script.js
+++ javascript/script.js
@@ -1,104 +1,115 @@
-    const informationBar = document.querySelector(".information_bar")
-    const navigator = document.querySelectorAll(".navigator li")
-    
-    const navigationLi = document.querySelectorAll(".navigator_bar li")
-
-
-    const wrapDiv = document.querySelectorAll("#wrap>div")
-
+const navigatorBar = document.querySelector(".navigator_bar")
+    const navigatorLi = document.querySelectorAll(".navigator i")
+    const text = document.querySelector(".blind_letter");
+    const textTitle = document.querySelector(".blind_letter h2");
+    const textSpan = document.querySelectorAll(".blind_letter p");
+   const  svg = document.querySelector("#svgExample")
+   const effectImg = document.querySelector(".landing_effect img")
+   const landingBtn  = document.querySelector(".landing_text .button")
+    let i = 0;
     let interval;
-    let i=0;
-    var offset = 0;
-    let counter=0;
-    let sectionCounter=0;
-
+    
     window.addEventListener("load", handleLoading)
     document.addEventListener("scroll", handleScroll)
-    document.addEventListener('wheel', handleWheel)      
+    landingBtn.addEventListener("click", handleClick)
 
-    function handleWheel(event){
-        counter++;
-            var gosectioncount = counter%5
-          
-        if(event.wheelDelta>0){
-            
-            if(gosectioncount == 4 && sectionCounter>0)sectionCounter--;
-            console.log("위로",gosectioncount,sectionCounter,wrapDiv)
-          
-        }
-        if(event.wheelDelta<0){
-            
-            if(gosectioncount == 4 && sectionCounter<5)sectionCounter++;
-            console.log("아래",gosectioncount,sectionCounter,wrapDiv)
-        }
 
-        let wheeling = wrapDiv[sectionCounter].animate([
-
-            {opacity:0},
-            {opacity:1}
-        ])
+    function handleClick(){
+      landingBtn.style.display="none"
+      text.animate([
+         {opacity:1},{opacity:0}
+      ],{duration:5000,
+         easing:"ease",
+         fill: "forwards",});
+      svg.animate([
+         {transform: "scale(40)",opacity: 0},
+         {transform: "scale(1)",opacity: 1}
+      ], {duration:5500,
+         easing:"ease-out",
+         fill: "forwards",
+         delay:-1800})
+         
+       setTimeout(startWeb,3700)
     }
+     function startWeb(){
+       svg.animate([
+          {transform:"translateY(" + 0 + "px)"},
+          {transform:"translateY(" + -100 + "px)"}
+             ],
+             {duration:1000,
+             fill: "forwards",
+             easing:"linear"});
+      navigatorBar.animate([
+         {transform:"translateX(" + -100 + "px)"},
+         {transform:"translateX(" + 0 + "px)"}
+            ],
+            {duration:800,
+            fill: "forwards",
+            easing:"linear",
+            delay:800
+            });   
+         }
 
-  
-       
-  
-      
-
-
-
-
-        
-
-
-    function handleLoading(){
-        setTimeout(delayNavi,7000)
-        }
-    function delayNavi (){
-        interval = setInterval(goNavi,400)
-    }
-    function goNavi(){
-        let navi = navigationLi[i].animate([
-            {
-                transform:"translateY(" + -50 + "px)"
-            },{
-                transform:"translateY(" + 0 + "px)"
-            }
-        ],400);
-        navigationLi[i].style.transform="translateY(0px)"
-        i++;
-        if(i > 3) {
-            console.log("종료");
-            clearInterval(interval);
-        };
-    }
-
-
+    function handleLoading(event){
+       svg.style.opacity=0;
+       landingBtn.style.opacity=0;
+       svg.style.display="block";
+       landingBtn.style.display="block"
+       textTitle.animate([
+        {transform:"translateY(" + 200 + "px)"},
+        {transform:"translateY(" + 0 + "px)"}
+         ],
+         {duration:1500,
+         fill: "forwards",
+         easing:"ease-out"});
+    setTimeout(startInterval,1500)
+   }
+   function startInterval(){
+      interval = setInterval(textspan,500)
+   }
+   function textspan(){
+      textSpan[i].animate([
+         {transform:"translateY(" + 200 + "px)"},
+         {transform:"translateY(" + 0 + "px)"}
+            ],
+            {duration:800,
+            fill: "forwards",
+            easing:"ease-out"});
+      i++;  
+      if(i === 2 ){
+         clearInterval(interval);
+         setTimeout( twinkle,4500)
+      }
+      function  twinkle(){
+         landingBtn.animate([
+            {opacity:0},{opacity:1},{opacity:0}
+         ],{duration:2000,
+            iterations: Infinity,
+            fill: "forwards"});
+      }
+   }
 
     function handleScroll(event){
-
-          if (window.pageYOffset>=innerHeight) {
-            informationBar.style.position="fixed";
-          }else{
-            informationBar.style.position="absolute";
+      console.log(svg.clientHeight)
+          if (window.pageYOffset<=innerHeight&&window.pageYOffset >= 0) {
+            navigatorLi.forEach((navi)=>navi.classList.remove("color"))
+            navigatorLi[0].classList.add("color")
+         
           }
          if(window.pageYOffset <= innerHeight*1+150 && window.pageYOffset >= innerHeight*1-150){
-            navigator.forEach((navi)=>navi.classList.remove("color"))
-            navigator[0].classList.add("color")
+            navigatorLi.forEach((navi)=>navi.classList.remove("color"))
+            navigatorLi[1].classList.add("color")
          }
          if(window.pageYOffset <= innerHeight*2+150 && window.pageYOffset >= innerHeight*2-150){
-            navigator.forEach((navi)=>navi.classList.remove("color"))
-            navigator[1].classList.add("color")
+            navigatorLi.forEach((navi)=>navi.classList.remove("color"))
+            navigatorLi[2].classList.add("color")
          }
          if(window.pageYOffset <= innerHeight*3+150 && window.pageYOffset >= innerHeight*3-150){
-            navigator.forEach((navi)=>navi.classList.remove("color"))
-            navigator[2].classList.add("color")
+            navigatorLi.forEach((navi)=>navi.classList.remove("color"))
+            navigatorLi[3].classList.add("color")
          }
          if(window.pageYOffset <= innerHeight*4+150 && window.pageYOffset >= innerHeight*4-150){
-            navigator.forEach((navi)=>navi.classList.remove("color"))
-            navigator[3].classList.add("color")
+            navigatorLi.forEach((navi)=>navi.classList.remove("color"))
+            navigatorLi[4].classList.add("color")
          }
     }
-     
-    let guage = {
-        startpos: 0, //pipe가 시작하는 위치
-      };
